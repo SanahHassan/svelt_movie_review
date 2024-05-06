058ef128f6e90fc21554f4f548a289a384b239a8
SanahHassan <161712884+SanahHassan@users.noreply.github.com>
2024-05-06 21:22:42 +0530

    Add files via upload

--- src/components/MoviePoster.svelte
+++ src/components/MoviePoster.svelte
@@ -5,7 +5,7 @@
 <div >
     <div >
    <a class="flex items-center justify-center" href={"/reviews/" +JSON.stringify(eachMovie)}>
-    <img class="flex items-center justify-center object-cover object-center w-60 h-80 max-w-full rounded-lg" src="../src/lib/images/{eachMovie.movie_id}.jpeg" alt={eachMovie.movie_name}>
+    <img class="flex items-center justify-center object-cover object-center w-60 h-80 max-w-full rounded-lg" src="/images/{eachMovie.movie_id}.jpeg" alt={eachMovie.movie_name}>
     </a> 
 </div>
 
