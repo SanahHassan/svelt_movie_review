34eef988f5ab7509006a1aaf22a03b34a7037055
SanahHassan <161712884+SanahHassan@users.noreply.github.com>
2024-05-06 14:01:57 +0530

    Add files via upload

--- src/routes/search/[slug]/+page.svelte
+++ src/routes/search/[slug]/+page.svelte
@@ -1,15 +1,19 @@
+
+
 <script lang="ts">
     import HomePage from '../../../components/HomePage.svelte';
     import movieList from '$lib/data/movieData.json';
+    import Search from '../../../components/Search.svelte';
     export let allMovies = movieList.movies;
 
     import { page } from '$app/stores';
-    
-    const searchParam = $page.params.slug.toLowerCase();
-
     export let movies: any[] = [];
-
-    allMovies.forEach(function(item){
+    export let searchParam = "";
+    
+    $:{
+        movies = [];
+        searchParam = $page.params.slug.toLowerCase();
+        allMovies.forEach(function(item){
 if(item.genre.toLowerCase().includes(searchParam) || item.movie_name.toLowerCase().includes(searchParam)){
     movies.push(item);
 }
@@ -18,10 +22,13 @@ if(item.genre.toLowerCase().includes(searchParam) || item.movie_name.toLowerCase
     });
 
     console.log(movies);
+    }
+  
 
 </script>
 
 <section>
+    <Search />
     
     {#if movies.length > 0}
     <p class="flex justify-center pb-4 pl-6 font-movieTitle font-normal text-slate-400 text-2xl">
