c4fef38cb0f1dc80107bda0c174638a0c3416282
SanahHassan <161712884+SanahHassan@users.noreply.github.com>
2024-05-07 19:46:38 +0530

    Add files via upload

--- src/components/MoviePoster.svelte
+++ src/components/MoviePoster.svelte
@@ -2,19 +2,21 @@
     export let eachMovie;
 </script>
 
+<a class="flex items-center justify-center" href={"/reviews/" +JSON.stringify(eachMovie)}>
 <div >
     <div >
-   <a class="flex items-center justify-center" href={"/reviews/" +JSON.stringify(eachMovie)}>
+  
     <img class="flex items-center justify-center object-cover object-center w-60 h-80 max-w-full rounded-lg" src="/images/{eachMovie.movie_id}.jpeg" alt={eachMovie.movie_name}>
-    </a> 
-</div>
-
-    <div >
-        <p class="flex justify-center items-center font-movieTitle font-normal text-2xl">{eachMovie.movie_name}</p> 
+  
     </div>
 
     <div >
-        <p class="flex justify-center items-center font-normal text-xl">{eachMovie.release_date}</p> 
+        <p class="flex justify-center items-center font-moviePoster font-normal text-2xl hover:text-slate-100">{eachMovie.movie_name}</p> 
+    </div>
+
+    <div >
+        <p class="flex justify-center items-center font-normal text-xl hover:text-slate-100">{eachMovie.release_date}</p> 
     </div>
   
 </div>
+</a> 
--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -9,10 +9,10 @@
 
 <div class="p-3 flex items-center justify-center">
     <input bind:value={searchInput} type="text" placeholder="Search by name or genre..."
-    class="h-8 text-slate-600 focus:text-slate-600 focus:font-movieTitle font-movieTitle rounded-full p-5  border  focus:border-gray-300 focus:border-teal focus:ring-0 focus:outline-none bg-gray-300 focus:bg-gray-200"/>
+    class="text-xl focus:text-xl text-slate-500 focus:text-slate-500 focus:font-searchBar font-searchBar rounded-full p-3  border  focus:border-gray-300 focus:border-teal focus:ring-0 focus:outline-none bg-gray-300 focus:bg-gray-200"/>
 
     {#if searchInput}
-    <button on:click={search}><div class="text-2xl">🔍</div></button>
+    <button on:click={search}><div class="text-3xl">🔍</div></button>
     {/if}
     
 </div>
